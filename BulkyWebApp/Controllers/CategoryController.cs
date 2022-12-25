ea06bdfc8c86173607c45cedda2d7b970c875c80
naGiX1212 <joeeel.8@hotmail.com>
2022-12-25 07:05:01 -0300

    Add project files.

---
+++ BulkyWebApp/Controllers/CategoryController.cs
@@ -0,0 +1,98 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        public readonly ICategoryRepository _db;
+        public CategoryController(ICategoryRepository db)
+        {
+            _db = db;
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+            var categoryFromDb = _db.GetFirstOrDefault(u=>u.Id==id);
+            if (categoryFromDb == null)
+                return NotFound();
+            return View(categoryFromDb);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePOST(int id)
+        {
+            var obj = _db.GetFirstOrDefault(u => u.Id == id);
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            _db.Remove(obj);
+            _db.Save();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.GetAll();
+            return View(objCategoryList);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id==0)
+                return NotFound();
+
+            var categoryFromDbFirst = _db.GetFirstOrDefault(u=>u.Id==id);
+            if (categoryFromDbFirst == null)
+                return NotFound();
+            return View(categoryFromDbFirst);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order Cannot Match Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Update(obj);
+                _db.Save();
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
+
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order Cannot Match Name");
+            }
+            if(ModelState.IsValid)
+            {
+                _db.Add(obj);
+                _db.Save();
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
+           
+        }
+
+    }
+}
